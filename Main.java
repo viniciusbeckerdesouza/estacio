265c96ef7b309792e6f216c47cbac24c4707d0b6
Vinícius Becker de Souza <>
2021-05-10 16:28:53 +0000

    inclui enhanced for

--- Main.java
+++ Main.java
@@ -1,8 +1,10 @@
-class Repete3 {
-  public static void main(String[] args){
-    for(int i=1; i<11; i++){
-      System.out.println("Contando: " + i);
-    }
-  }
+class EnhancedFor {
+public static void main(String[] args){
+ //declarando um vetor inteiro contendo uma lista de números
+int[] numeros = {1,2,3,4,5,6,7,8,9,10};
+//o for abaixo percorre o vetor e mostra na tela cada um dos números
+for (int item : numeros) {
+ System.out.println("Contando: " + item);
+ }
 }
-
+}
